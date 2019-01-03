30ada5332ab10c8caaa6e20bb767c4dc33c66725
wisog <cesar.vela@gmail.com>
2019-01-02 19:56:35 -0600

    added model dtos using jackson annotations

---
+++ src/main/java/com/stokkur/exam/demoAccounts/dto/AccountDto.java
@@ -0,0 +1,111 @@
+package com.stokkur.exam.demoAccounts.dto;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "username",
+        "email",
+        "password",
+        "create_time",
+        "role"
+})
+public class AccountDto implements Serializable
+{
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+    @JsonProperty("create_time")
+    private String createTime;
+    @JsonProperty("role")
+    private RoleDto role;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    private final static long serialVersionUID = 5360269125289502719L;
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("username")
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonProperty("username")
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @JsonProperty("email")
+    public String getEmail() {
+        return email;
+    }
+
+    @JsonProperty("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty("password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @JsonProperty("create_time")
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    @JsonProperty("create_time")
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    @JsonProperty("role")
+    public RoleDto getRole() {
+        return role;
+}
+
+    @JsonProperty("role")
+    public void setRole(RoleDto role) {
+        this.role = role;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
